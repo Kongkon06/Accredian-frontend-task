20c85cfd5bc6d4e28912755a8316b3a78b030ae9
Kongkon Bora <kbora3525@gmail.com>
2025-02-26 12:03:59 +0530

    fixing top bar

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,6 +1,6 @@
 export function Topbar(){
     return (
-        <div className="w-full p-4 flex justify-center bg-[#EEF5FF] text-sm md:text-base">
+        <div className="w-full p-4 flex justify-center bg-[#EEF5FF] text-[10px] lg:text-base">
           Navigate your ideal Career path with tailored expert advice
           <span className="text-blue-600 ml-1 font-medium cursor-pointer hover:underline">
             Contact expert
